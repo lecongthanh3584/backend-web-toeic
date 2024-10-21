530643ab479a484805811a9a25a4d1efa4bab41f
Lê Công Thành <93207303+lecongthanh3584@users.noreply.github.com>
2024-10-21 19:47:37 +0700

    Delete src/main/java/com/backend/spring/test directory
    Fix folder temporary

--- src/main/java/com/backend/spring/test/CreditLimitExceededException.java
+++
@@ -1,7 +0,0 @@
-package com.backend.spring.test;
-
-public class CreditLimitExceededException extends RuntimeException {
-    public CreditLimitExceededException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/backend/spring/test/DailyWithdrawalLimitExceededException.java
+++
@@ -1,7 +0,0 @@
-package com.backend.spring.test;
-
-public class DailyWithdrawalLimitExceededException extends RuntimeException {
-    public DailyWithdrawalLimitExceededException(String message) {
-        super(message);
-    }
-}
